37c821986997865530d602b51202a3376dfef891
Luan <luaan.carlos@hotmail.com>
2025-02-28 23:20:56 -0300

    comecando projeto ignitegym

---
+++ ignitegym/App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar, StyleSheet, View } from 'react-native';
+
+import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto'
+
+import { GluestackUIProvider, Text } from '@gluestack-ui/themed';
+
+export default function App() {
+  const [fontsLoaded] =useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <GluestackUIProvider>
+      <View style={{ 
+        flex: 1, 
+        alignItems: 'center', 
+        justifyContent: 'center',
+        backgroundColor: '#202024' 
+      }}>
+        <StatusBar barStyle="light-content" backgroundColor='transparent' translucent />
+        {fontsLoaded ? <Text color='white'>Home</Text > : <View />}
+      </View>
+    </GluestackUIProvider>
+  );
+}
